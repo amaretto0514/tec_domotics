211eaad687dca8ab19237ec6426f9ce982c69b57
Víctor Agustín Villalta Delgado <116325350+amaretto0514@users.noreply.github.com>
2023-06-20 16:14:45 -0500

    php correo

---
+++ enviar.php
@@ -0,0 +1,42 @@
+<?php 
+
+$correo = $_POST['correo'];
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+
+//echo $correo . " " . $nombre . " " . $mensaje;
+
+
+$destinatario = "agustinvt1@gmail.com";
+$asunto = "Envio de correo de prueba con PHP"; 
+$cuerpo = '
+    <html> 
+        <head> 
+            <title>Prueba de envio de correo</title> 
+        </head>
+
+        <body> 
+            <h1>Solicitud de contacto desde correo de prueba !  </h1>
+            <p> 
+                Contacto:  '.$nombre . ' - ' . $asunto .'  <br>
+                Contacto:  '.$telefono . ' -   <br>
+                Mensaje: '.$mensaje.' 
+            </p> 
+        </body>
+    </html>
+';
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF8\r\n"; 
+
+//dirección del remitente
+
+$headers .= "FROM: $nombre <$correo>\r\n";
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+echo "Correo enviado"; 
+?> 
+
+<a href="index.html">Volver a inicio</a>
